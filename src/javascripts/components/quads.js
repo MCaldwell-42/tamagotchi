9a57d17efacab99bccdf1d969e2e6d0630917d5c
Matthew Caldwell <MCaldwell4242@gmail.com>
2019-04-21 15:23:28 -0500

    status bar working

--- src/javascripts/components/quads.js
+++ src/javascripts/components/quads.js
@@ -1,9 +1,8 @@
 import eats from './eat';
 
+
 const quad1 = () => {
-  eats.buildEats();
-  document.getElementById('healthy').addEventListener('click', eats.eatHealthy);
-  document.getElementById('unhealthy').addEventListener('click', eats.eatJunk);
+  eats.eatBuilder();
 };
 
 
